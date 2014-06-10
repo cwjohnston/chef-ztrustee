89bce8b5e4197d378b2931b6836ab0eb0b3b9d6d
Cameron Johnston <cameron@rootdown.net>
2014-06-10 08:32:40 -0600

    [apache] remove superfluous default site configuration bits

--- recipes/_server_apache.rb
+++ recipes/_server_apache.rb
@@ -1,26 +1,23 @@
+node.set[:apache][:default_site_enabled] = false
+
 %w{ default mod_ssl mod_rewrite mod_wsgi mod_proxy_http }.each do |r|
   include_recipe "apache2::#{r}"
 end
 
-template "#{node[:apache][:dir]}/sites-available/ztrustee.conf" do
-  source 'apache-ztrustee.conf.erb'
-  owner node[:apache][:user]
-  group node[:apache][:group]
-  mode 0640
-end
+%w( ztrustee.conf ztrusteehkp.conf ).each do |zsite|
+  template "#{node[:apache][:dir]}/sites-available/#{zsite}" do
+    source "apache-#{zsite}.erb"
+    owner node[:apache][:user]
+    group node[:apache][:group]
+    mode 0640
+  end
 
-template "#{node[:apache][:dir]}/sites-available/ztrusteehkp.conf" do
-  source 'apache-ztrusteehkp.conf.erb'
-  owner node[:apache][:user]
-  group node[:apache][:group]
-  mode 0640
-end
-
-%w( ztrustee ztrusteehkp ).each do |zsite|
+  # delete default site configs dropped off by package
   file "/etc/httpd/conf.d/#{zsite}" do
     action :delete
   end
 
+  # enable ztrustee site configs dropped off by this recipe
   apache_site zsite do
     action :enable
   end
